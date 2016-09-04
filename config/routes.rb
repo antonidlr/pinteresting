26e595b8c666ec4f1ea147a151d0494f9c6ff3aa
Antonio delosrios <antoniodelosrios22@gmail.com>
2016-09-04 20:44:27 -0300

    Updating Bundle

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#home"
   get "about" => "pages#about" #creates about_path
-  get "team" => "pages#team" #creates team_path
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
